ab33c13fc06a2c0b342081de84f601fb5dd2469a
Igor Frank Moreno <91378487+igorzfrank@users.noreply.github.com>
2023-09-11 20:36:43 -0300

    Fim do dia

--- Origamid/Javascript ES6/04-efeitos-no-dom/05-dropdown-menu/js/script.js
+++ Origamid/Javascript ES6/04-efeitos-no-dom/05-dropdown-menu/js/script.js
@@ -3,9 +3,13 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropDownMenu from "./modules/dropdown-menu.js";
 
 initTabNav();
 initAccordion();
 initScrollSuave();
 initAnimacaoScroll();
 initModal();
+initTooltip();
+initDropDownMenu();

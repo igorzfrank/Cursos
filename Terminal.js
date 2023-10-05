72527102a9fe10f897881c0e816cb99690a54f8e
Igor Frank Moreno <91378487+igorzfrank@users.noreply.github.com>
2023-10-05 20:24:47 -0300

    forms

--- Terminal.js
+++ Terminal.js
@@ -1,17 +1,7 @@
-finalQuestions = []
+teste = 10
 
-while (finalQuestions.length < 5) {
-    let randomNumber = Math.floor(Math.random() * (20 - 1) + 1)
-    if (!finalQuestions.includes(randomNumber)) {
-        finalQuestions.push(randomNumber)
-    }
-}   
-
-while (finalQuestions.length < 10) {
-    let randomNumber = Math.floor(Math.random() * (47 - 20) + 20)
-    if (!finalQuestions.includes(randomNumber)) {
-        finalQuestions.push(randomNumber)
-    }
-}  
-
-console.log(finalQuestions)
+if(teste) {
+    console.log('true')
+} else {
+    console.log('false')
+}
